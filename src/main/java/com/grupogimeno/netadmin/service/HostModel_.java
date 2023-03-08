b03039f0f29530093cccd52667991cb59a47b256
Ivan Duart <ivan.duart93@gmail.com>
2023-03-08 19:19:06 +0100

    Cambios

---
+++ src/main/java/com/grupogimeno/netadmin/service/HostModel_.java
@@ -0,0 +1,11 @@
+package com.grupogimeno.netadmin.service;
+
+import javax.persistence.metamodel.SingularAttribute;
+
+import com.grupogimeno.netadmin.models.HostModel;
+
+public class HostModel_ {
+
+    public static SingularAttribute<? super HostModel, String> host;
+
+}
--- src/main/java/com/grupogimeno/netadmin/service/hostService.java
+++ src/main/java/com/grupogimeno/netadmin/service/hostService.java
@@ -1,6 +1,8 @@
 package com.grupogimeno.netadmin.service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import javax.transaction.Transactional;
 
@@ -10,7 +12,6 @@ import org.springframework.stereotype.Service;
 
 import com.grupogimeno.netadmin.criteria.HostCritera;
 import com.grupogimeno.netadmin.models.HostModel;
-import com.grupogimeno.netadmin.models.HostModel_;
 import com.grupogimeno.netadmin.repositories.HostRepository;
 
 import io.github.jhipster.service.QueryService;
@@ -26,14 +27,34 @@ public class hostService extends QueryService<HostModel>{
         List<HostModel> hosts = hostRepository.findAll(specification);
         return hosts;
     }
-    private Specification<HostModel> createSpecification (HostCritera hostCritera) {
-        Specification<HostModel> specification = Specification.where (null);
-        if (criteria != null ){
-            if(criteria.getHost () != null ){
-                specification = specification.and(buildStringSpecification(criteria.getHost(), HostModel_.host));
+    /**
+     * @param hostCritera
+     * @return
+     */
+    private Specification<HostModel> createSpecification(HostCritera hostCriteria) {
+        Specification<HostModel> specification = Specification.where(null);
+        if (hostCriteria != null) {
+            if (hostCriteria.getHost() != null) {
+                specification = specification.and(buildStringSpecification(hostCriteria.getHost(), HostModel_.host));
             }
         }
         return specification;
+    }
+    
+    public ArrayList<HostModel> getHosts() {
+        return null;
+    }
+    public HostModel saveHost(HostModel host) {
+        return null;
+    }
+    public Optional<HostModel> getByIdHost(Integer id_host) {
+        return null;
+    }
+    public ArrayList<HostModel> findByHostBrand(Integer host_brand) {
+        return null;
+    }
+    public boolean deleteHost(Integer id_host) {
+        return false;
     }
 
 }
